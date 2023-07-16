a0230cf379d143dda80632ab1ca49adaf2338d05
Relrige <stasp870@gmail.com>
2023-07-16 11:59:58 +0300

    comment hw

--- my-app/src/components/category/edit/CategoryEditPage.tsx
+++ my-app/src/components/category/edit/CategoryEditPage.tsx
@@ -15,6 +15,7 @@ const CategoryEditPage = () => {
     const {id} = useParams();
     const [oldImage, setOldImage] = useState<string>("");
 
+    // Ініціалізуємо початкові значення форми редагування категорії
     const init: ICategoryEdit = {
         id: id ? Number(id) : 0,
         name: "",
@@ -23,13 +24,15 @@ const CategoryEditPage = () => {
     };
 
     const onFormikSubmit = async (values: ICategoryEdit) => {
-        //console.log("Send Formik Data", values);
         try {
+            // Відправляємо дані форми на сервер
             const result = await http_common.post(`api/category/edit/${id}`, values, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 },
             });
+
+            // Перенаправляємо користувача після успішного оновлення категорії
             navigate("../..");
         } catch {
             console.log("Server error");
@@ -44,12 +47,13 @@ const CategoryEditPage = () => {
     const {values, handleChange, handleSubmit, setFieldValue} = formik;
 
     useEffect(() => {
+        // Отримуємо дані про категорію з сервера за допомогою її ідентифікатора (id)
         http_common.get<ICategoryItem>(`api/category/${id}`)
             .then(resp => {
                 const {data} = resp;
+
+                // Встановлюємо отримані значення у відповідні поля форми
                 setFieldValue("name", data.name);
-                //setFieldValue("image", data.image);
-                //посилання на фото, яке було у категорії
                 setOldImage(`${APP_ENV.BASE_URL}/uploads/300_${data.image}`);
                 setFieldValue("description", data.description);
             });
@@ -61,7 +65,7 @@ const CategoryEditPage = () => {
         {
             const file = files[0];
             if(file) {
-                //Перевірка на тип обраного файлу - допустимий тип jpeg, png, gif
+                // Перевірка типу обраного файлу - допустимі типи: jpeg, png, gif
                 const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
                 if (!allowedTypes.includes(file.type)) {
                     alert("Не допустимий тип файлу");
@@ -85,30 +89,37 @@ const CategoryEditPage = () => {
                                value={values.name}
                                onChange={handleChange}
                                name="name"/>
+                               
+                               // Це поле вводу для назви категорії. Значення поля зберігається в values.name, і при його зміні викликається функція handleChange.
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="image" className="form-label">
+                            //Це зображення, яке відображається у формі. Якщо значення values.image є нульовим (тобто зображення не вибране), тоді використовується imgView - посилання на старе зображення.
                             <img src={values.image==null ? imgView : URL.createObjectURL(values.image)}
                                  alt="фото"
                                  width={200}
                                  style={{cursor: "pointer"}}/>
                             <span className={"btn btn-danger"}>Змінить фото</span>
                         </label>
+                        // Це поле вводу файлу для вибору зображення. При зміні файлу викликається функція onChangeFileHandler.
                         <input type="file" className="form-control d-none" id="image"
                                onChange={onChangeFileHandler}
                                name="image"/>
+                               
+                               
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Опис</label>
+                        //Це поле вводу для опису категорії. Значення поля зберігається в values.description, і при його зміні викликається функція handleChange.
                         <input type="text" className="form-control" id="description"
                                value={values.description}
                                onChange={handleChange}
                                name="description"/>
                     </div>
 
-                    <button type="submit" className="btn btn-primary">Зберегти</button>
+                    <button type="submit" className="btn btn-primary">Зберегти</button> //Це кнопка "Зберегти", яка виконується під час натискання. При натисканні виконується функція handleSubmit, яка обробляє дані форми.
                 </form>
             </div>
         </>
